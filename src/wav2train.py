74b41c8897e3fffe23614207cb1c036581f34b52
Ryan Hileman <lunixbochs@gmail.com>
2020-02-01 22:02:34 -0800

    use debug logging for target stderr

--- src/wav2train.py
+++ src/wav2train.py
@@ -63,7 +63,7 @@ def align(args):
             continue
         if 'Your CPU supports instructions' in line:
             continue
-        logging.error(line)
+        logging.debug(line)
     return (audio_file, aligned, linked_transcript)
 
 words_re = re.compile(r"[a-zA-Z']+")
