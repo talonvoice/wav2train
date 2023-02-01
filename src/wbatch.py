9422018393dfac666106e369823e286e8038bf75
Ryan Hileman <lunixbochs@gmail.com>
2023-02-01 08:18:16 -0800

    implement audio caching in wbatch

--- src/wbatch.py
+++ src/wbatch.py
@@ -1,12 +1,35 @@
+from multiprocessing import Pool
 from tempfile import NamedTemporaryFile
 import argparse
+import hashlib
 import os
 import subprocess
+import itertools
+
+from tqdm import tqdm
 
 base = os.path.dirname(__file__)
 wfilter = os.path.join(base, '..', 'wfilter')
 
-def batch_filter(outdir, set_name, lists, argv, merge=False):
+def cache_one(args):
+    line, cache_dir = args
+    length = len(line)
+    line = line.strip()
+    if not line:
+        return ''
+    _id, path, duration, text = line.split(' ', 3)
+    with open(path, 'rb') as f:
+        data = f.read()
+        h = hashlib.sha256(data).hexdigest()
+        cache_base1 = os.path.join(cache_dir, h[:2])
+        cache_base2 = os.path.join(cache_base1, h[2:4])
+        ext = path.rsplit('.', 1)[1]
+        cache_path = os.path.join(cache_base2, f"{h}.{ext}")
+        with open(cache_path, 'wb') as o:
+            o.write(data)
+    return _id, cache_path, duration, text, length
+
+def batch_filter(outdir, set_name, lists, argv, merge=False, cache=None):
     if not lists:
         return ''
     print('[+] {}'.format(set_name))
@@ -14,12 +37,27 @@ def batch_filter(outdir, set_name, lists, argv, merge=False):
         lstname = '{}.lst'.format(set_name)
         outlst = os.path.join(outdir, lstname)
         with NamedTemporaryFile('w', suffix='.lst') as tmp:
-            for lst in lists:
+            count = 0
+            for lst in tqdm(lists, desc='copy lists'):
                 with open(lst, 'r') as f:
-                    tmp.write(f.read().strip() + '\n')
+                    data = f.read().strip() + '\n'
+                    count += data.count('\n')
+                    tmp.write(data)
             tmp.flush()
-            with open(outlst, 'w') as out:
-                subprocess.check_call([wfilter, tmp.name, '--desc', lstname] + argv, stdout=out)
+            if cache is None:
+                with open(outlst, 'w') as out:
+                    subprocess.check_call([wfilter, tmp.name, '--desc', lstname] + argv, stdout=out)
+            else:
+                with NamedTemporaryFile('w+', suffix='.lst') as tmp2:
+                    subprocess.check_call([wfilter, tmp.name, '--desc', lstname] + argv, stdout=tmp2)
+                    tmp2.seek(0, os.SEEK_END)
+                    size = tmp2.tell()
+                    tmp2.seek(0, os.SEEK_SET)
+
+                    with open(outlst, 'w') as out:
+                        pool_iter = pool.imap(cache_one, zip(tmp2, itertools.repeat(cache)), chunksize=8)
+                        for _id, path, duration, text, length in tqdm(pool_iter, desc='cache', total=count):
+                            out.write(f"{_id} {path} {duration} {text}\n")
         return lstname
 
     datadir = os.path.commonprefix(lists)
@@ -55,9 +93,19 @@ def main(args, argv):
     valid_set = [os.path.join(datadir, lst) for lst in valid_set if lst]
 
     os.makedirs(outdir, exist_ok=True)
-    flags['--train'] = batch_filter(outdir, 'train', train_set, argv, merge=args.merge)
-    flags['--test']  = batch_filter(outdir, 'test',  test_set,  argv)
-    flags['--valid'] = batch_filter(outdir, 'valid', valid_set, argv)
+    if args.cache:
+        cache = args.cache
+        os.makedirs(cache, exist_ok=True)
+        for a in tqdm(range(256), desc='cache dirs'):
+            for b in range(256):
+                try:
+                    os.mkdir(os.path.join(cache, f"{a:02x}", f"{b:02x}"))
+                except FileExistsError:
+                    pass
+
+    flags['--train'] = batch_filter(outdir, 'train', train_set, argv, merge=args.merge, cache=args.cache)
+    flags['--test']  = batch_filter(outdir, 'test',  test_set,  argv, cache=args.cache)
+    flags['--valid'] = batch_filter(outdir, 'valid', valid_set, argv, cache=args.cache)
     flags['--datadir'] = outdir
 
     with open(os.path.join(outdir, 'flagsfile'), 'w') as f:
@@ -65,10 +113,13 @@ def main(args, argv):
             f.write('{}={}\n'.format(k, v))
 
 if __name__ == '__main__':
+    pool = Pool()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--flagsfile', help='input flagsfile path', type=str, required=True)
     parser.add_argument('--output',    help='output directory', type=str, required=True)
     parser.add_argument('--merge',     help='merge train into one list', action='store_true')
+    parser.add_argument('--cache',     help='cache audio to this directory', type=str, default=None)
 
     args, unknown = parser.parse_known_args()
     main(args, unknown)
